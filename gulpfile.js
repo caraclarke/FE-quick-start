fd1f4f20b4ca452d18ae8cc340cae9a2e61c776a
caraclarke <cara.clarke8@gmail.com>
2017-05-24 09:51:03 -0400

    add elements to index.html file
    add browsersync to gulp to auto update

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,27 @@
+var browserSync = require('browser-sync').create();
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+
+// browser live reload
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'app'
+    },
+  });
+});
 
 // scss to css
 gulp.task('sass', function() {
   return gulp.src('app/scss/**/*.+(scss|css)')
     .pipe(sass())
     .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
 });
 
 // watch
-gulp.task('watch', function() {
+gulp.task('watch', ['browserSync', 'sass'], function() {
   gulp.watch('app/scss/**/*.+(scss|css)', ['sass']);
 });
